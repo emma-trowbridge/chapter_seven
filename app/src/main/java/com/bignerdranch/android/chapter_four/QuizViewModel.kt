9c21ced91fe43d214b48a014be5bf332f987602a
emmat <emma.trowbridgee@gmail.com>
2024-09-27 12:29:41 -0500

    Commit - Final

--- app/src/main/java/com/bignerdranch/android/chapter_four/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/chapter_four/QuizViewModel.kt
@@ -1,11 +1,13 @@
 package com.bignerdranch.android.chapter_four
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
+const val  CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 
-class QuizViewModel:ViewModel() {
+class QuizViewModel(private val savedStateHandle: SavedStateHandle):ViewModel() {
 
     /*init {
         Log.d(TAG, "ViewModel instance created")
@@ -25,7 +27,9 @@ class QuizViewModel:ViewModel() {
         Question(R.string.question_asia, true)
     )
 
-    private var currentIndex = 0
+    private var currentIndex
+        get() = savedStateHandle.get(CURRENT_INDEX_KEY)?: 0 //grabbed what it was coming into constructor
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value) //updated it with set value
 
     val currentQuestionAnswer:Boolean
         get() = questionBank[currentIndex].answer
